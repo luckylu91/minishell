452fd5a18bccd382819b47f062705061473d9f80
LZins <zins.lucas@gmail.com>
2021-03-24 10:16:26 +0100

    added getenv in example

--- ast_example.c
+++ ast_example.c
@@ -13,6 +13,20 @@ t_ast	*new_ast_str(char *str)
 	return (ast);
 }
 
+t_ast	*new_ast_unary(char *name, t_ast *target)
+{
+	t_ast					*ast;
+	struct s_unary_expr	*expr;
+
+	ast = calloc(1, sizeof(t_ast));
+	ast->type = unary_expr;
+	expr = &(ast->expr.unary_expr);
+	expr->op_name = strdup(name);
+	expr->target = target;
+	return (ast);
+}
+
+
 t_ast	*new_ast_binary(char *name, t_ast *left, t_ast *right)
 {
 	t_ast					*ast;
@@ -35,7 +49,6 @@ char	*ft_strjoin(char *s1, char *s2)
 
 	len_tot = strlen(s1) + strlen(s2);
 	res = calloc(len_tot + 1, sizeof(char));
-	fflush(stdout);
 	strcat(res, s1);
 	strcat(res, s2);
 	return (res);
@@ -43,9 +56,16 @@ char	*ft_strjoin(char *s1, char *s2)
 
 char	*full_concat_aggragation(t_ast *ast)
 {
-	char *res;
+	char				*res;
+	struct s_unary_expr	*uexpr;
 	if (ast->type == string_expr)
 		return (strdup(ast->expr.string_expr));
+	else if (ast->type == unary_expr)
+	{
+		uexpr = &(ast->expr.unary_expr);
+		if (strcmp(uexpr->op_name, "$") == 0 && uexpr->target->type == string_expr)
+			return (getenv(uexpr->target->expr.string_expr));
+	}
 	else if (ast->type == binary_expr)
 		return (
 			ft_strjoin(
@@ -53,16 +73,21 @@ char	*full_concat_aggragation(t_ast *ast)
 				full_concat_aggragation(ast->expr.binary_expr.right)
 			)
 		);
+	return (NULL);
 }
 
 int main()
 {
-	t_ast *ast;
+	t_ast	*ast;
+	char	*res;
 
 	ast = new_ast_binary(
 		"concatenate",
 		new_ast_str("lalali"),
-		new_ast_str("lilalo")
+		new_ast_unary("$", new_ast_str("PATH"))
 	);
-	printf("%s\n", full_concat_aggragation(ast));
+	res = full_concat_aggragation(ast);
+	if (!res)
+		printf("Aggregation returned NULL\n");
+	printf("%s\n", res);
 }
