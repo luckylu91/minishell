ea1b5b82af5e5e7c541407ca577b8d8c87513dab
Lucas Zins <lzins@z1r3p3.42lyon.fr>
2021-03-25 14:37:20 +0100

    pushing to pull

--- ast.h
+++ ast.h
@@ -1,15 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ast.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lzins <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/25 14:36:10 by lzins             #+#    #+#             */
+/*   Updated: 2021/03/25 14:36:22 by lzins            ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef AST_H
 #define AST_H
+
+#include "libft.h"
 
 typedef struct	s_ast
 {
 	enum e_type
 	{
-		string_expr, unary_expr, binary_expr, command_expr
+		string_expr, text_expr, redir_expr,
+		unary_expr, binary_expr, command_expr
 	}	type;
 	union u_expr
 	{
-		char				*string_expr;
+		t_block				*string_expr;
+		t_list				*text_expr; // t_block list
+		struct				s_redir_expr
+		{
+			int				fildes;
+			char			*redir_op;
+			char			*file_name;
+		}					redir_expr;
 		struct				s_unary_expr
 		{
 			char			*op_name;
@@ -23,9 +45,8 @@ typedef struct	s_ast
 		}					binary_expr;
 		struct				s_command_expr
 		{
-			char			*cmd_name;
-			struct t_list	*arg_list;
-			struct t_list	*redir_list;
+			t_list			*text_list; // ast::text_expr list
+			t_list			*redir_list; // ast::redir_expr list
 		}					command_expr;
 	}						expr;
 
